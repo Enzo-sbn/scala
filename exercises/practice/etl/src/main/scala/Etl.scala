c8259e7261dff3a98a79c9d2d5b92ff3a74dddbf
enzosabino <sabinoenzo2001@gmail.com>
2023-06-05 19:12:26 +0200

    exercise Etl

--- exercises/practice/etl/src/main/scala/Etl.scala
+++ exercises/practice/etl/src/main/scala/Etl.scala
@@ -1,3 +1,7 @@
 object Etl {
-  def transform(scoreMap: Map[Int, Seq[String]]): Map[String, Int] = ???
+  def transform(scoreMap: Map[Int, Seq[String]]): Map[String, Int] =
+    for {
+      (score, strings) <- scoreMap
+      string <- strings
+    } yield (string.toLowerCase(), score)
 }
